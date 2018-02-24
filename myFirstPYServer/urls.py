75e80c8770db0e3b99e661a6c4c4f43f93be364a
公司 <951827860@qq.com>
2018-02-24 11:23:35 +0800

    配置静态文件夹

--- myFirstPYServer/urls.py
+++ myFirstPYServer/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.conf.urls import url,include
 
+from django.conf.urls.static import static
+from django.conf import settings
+
 urlpatterns = [
     url('admin/', admin.site.urls),
     url('^masterWeiBo/', include('masterWeiBo.urls')),
-]
+]+static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
